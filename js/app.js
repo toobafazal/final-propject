6ae83f6d5de60cb96e4527e97437f175833ef34a
tooba fazal <toobafazal75@gmail.com>
2021-08-15 13:45:16 -0700

    project

---
+++ js/app.js
@@ -0,0 +1,52 @@
+//oourse slick 
+$('.slider').slick({
+  dots: true,
+  infinite: false,
+  speed: 300,
+  autoplay:true,
+  slidesToShow: 3,
+  slidesToScroll: 2,
+  content:">",
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+});
+// slick end 	
+
+
+
+
+
+
+
+
+
+
+
+
+
